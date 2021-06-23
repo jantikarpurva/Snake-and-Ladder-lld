7e68fa582861430d9227563e6f6d7b7b5165a1e1
purva jantikar <jantikarpurva@gmail.com>
2021-06-23 13:44:29 +0530

    Change 2

--- src/SnakeAndLadder/services/DiceService.java
+++ src/SnakeAndLadder/services/DiceService.java
@@ -2,7 +2,7 @@ package SnakeAndLadder.services;
 
 public class DiceService {
     public static int rollDice(int numOfDice){
-        int min = 1;
+        int min = numOfDice;
         int max = numOfDice*6; // one dice can have max value as 6 eg = if 2 dice are there 2*6 = 12
         return (int) (Math.random() *(max-min+1)+min); // formula to create random number
     }
